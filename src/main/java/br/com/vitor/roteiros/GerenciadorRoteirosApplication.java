db7aa190e588059aa23e4ef029600c15e0bdc97e
Vitor Wilton <melki9343@gmail.com>
2025-08-25 19:24:39 -0300

    Projeto funcional com a API do google,configuração de segurança será implementada em outra branch

--- src/main/java/br/com/vitor/roteiros/GerenciadorRoteirosApplication.java
+++ src/main/java/br/com/vitor/roteiros/GerenciadorRoteirosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciadorRoteirosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorRoteirosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorRoteirosApplication.class, args);
+    }
 
 }
